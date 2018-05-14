eb0990ab02aa2880dc2b9701820f8cc7ec7a5c0a
David R. Bild <david.bild@xaptum.com>
2018-05-14 23:02:37 +0000

    xtt: add as external package as v0.7.2

---
+++ br2-ext-xaptum/package/xtt/xtt.mk
@@ -0,0 +1,29 @@
+################################################################################
+#
+# XTT
+#
+################################################################################
+
+XTT_VERSION = v0.7.2
+XTT_SITE = $(call github,xaptum,xtt,$(XTT_VERSION))
+XTT_LICENSE = Apache-2.0
+XTT_LICENSE_FILES = LICENSE
+XTT_INSTALL_STAGING = YES
+XTT_SUPPORTS_IN_SOURCE_BUILD = NO
+
+XTT_DEPENDENCIES += ecdaa libsodium
+XTT_DEPENDENCIES += $(if $(BR2_PACKAGE_XTT_TPM_SUPPORT),xaptum-tpm,)
+
+ifeq ($(BR2_PACKAGE_XTT_TPM_SUPPORT),y)
+	XTT_CONF_OPTS += -DUSE_TPM=ON
+else
+	XTT_CONF_OPTS += -DUSE_TPM=OFF
+endif
+
+ifeq ($(BR2_PACKAGE_XTT_UTILS),y)
+	XTT_CONF_OPTS += -DBUILD_UTILS=ON
+else
+	XTT_CONF_OPTS += -DBUILD_UTILS=OFF
+endif
+
+$(eval $(cmake-package))
