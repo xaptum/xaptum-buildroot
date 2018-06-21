d505966ba877675ef21d792a881909e06bbbcdf7
David R. Bild <david.bild@xaptum.com>
2018-06-21 19:40:19 +0000

    xaptum-tpm: update to v0.5.5

--- buildroot-external-xaptum/package/xaptum-tpm/xaptum-tpm.mk
+++ buildroot-external-xaptum/package/xaptum-tpm/xaptum-tpm.mk
@@ -4,10 +4,16 @@
 #
 ################################################################################
 
-XAPTUM_TPM_VERSION = v0.5.4
+XAPTUM_TPM_VERSION = v0.5.5
 XAPTUM_TPM_SITE = $(call github,xaptum,xaptum-tpm,$(XAPTUM_TPM_VERSION))
 XAPTUM_TPM_LICENSE = Apache-2.0
 XAPTUM_TPM_LICENSE_FILES = LICENSE
 XAPTUM_TPM_INSTALL_STAGING = YES
+
+ifeq ($(BR2_PACKAGE_XAPTUM_TPM_UTILS),y)
+	XAPTUM_TPM_CONFIG_OPTS += -DBUILD_UTILS=ON
+else
+	XAPTUM_TPM_CONFIG_OPTS += -DBUILD_UTILS=OFF
+endif
 
 $(eval $(cmake-package))
