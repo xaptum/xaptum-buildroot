b981de8acb844b9f6dbf269944f015c8cdf3d44e
David R. Bild <david.bild@xaptum.com>
2019-02-12 23:55:37 +0000

    mender-certs: add package to install mender certs

---
+++ buildroot-external-xaptum/package/mender-certs/mender-certs.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# MENDER_CERTS
+#
+################################################################################
+
+MENDER_CERTS_VERSION = 931b98be3508e0d78dd49ebebc0385f5362b591f
+MENDER_CERTS_SITE = ssh://git@github.com/xaptum/xaprc.git
+MENDER_CERTS_SITE_METHOD = git
+MENDER_CERTS_SUPPORTS_IN_SOURCE_BUILD = NO
+
+MENDER_CERTS_SUBDIR = mender-certs
+
+ifeq ($(BR2_PACKAGE_MENDER_CERTS_DEV),y)
+	MENDER_CERTS_CONF_OPTS += -DMENDER_ENV=dev
+else ifeq ($(BR2_PACKAGE_MENDER_CERTS_PROD),y)
+	MENDER_CERTS_CONF_OPTS += -DMENDER_ENV=prod
+endif
+
+$(eval $(cmake-package))
