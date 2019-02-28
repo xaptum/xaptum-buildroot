55d88f20108347c15df7f1b8e8502458b5008a99
Daniel Berliner <daniel.berliner@xaptum.com>
2019-02-28 21:09:51 +0000

    Enable captived service

--- buildroot-external-xaptum/package/captived/captived.mk
+++ buildroot-external-xaptum/package/captived/captived.mk
@@ -11,5 +11,11 @@ CAPTIVED_SUPPORTS_IN_SOURCE_BUILD = NO
 
 CAPTIVED_SUBDIR = captived
 
+define CAPTIVED_INSTALL_INIT_SYSTEMD
+        mkdir -p $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants
+        ln -fs ../../../../usr/lib/systemd/system/captived.service \
+                $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/captived.service
+endef
+
 $(eval $(cmake-package))
 
