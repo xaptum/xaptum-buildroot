96e3aedc6602a5dd764f90613965b9330d89c7eb
David R. Bild <david.bild@xaptum.com>
2019-02-28 20:17:31 -0600

    mender-identity: update to fix address path

--- buildroot-external-xaptum/package/mender-identity/mender-identity.mk
+++ buildroot-external-xaptum/package/mender-identity/mender-identity.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MENDER_IDENTITY_VERSION = aef4c1d606b79e1cdb514d3d0e9e166d558271c9
+MENDER_IDENTITY_VERSION = cd10f5a6fd8014bd9dc37f7f91ee9d3b72966309
 MENDER_IDENTITY_SITE = ssh://git@github.com/xaptum/xaprc.git
 MENDER_IDENTITY_SITE_METHOD = git
 MENDER_IDENTITY_SUPPORTS_IN_SOURCE_BUILD = NO
