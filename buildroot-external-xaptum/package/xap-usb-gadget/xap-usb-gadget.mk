838f0580d5d4a8f9baf4001cbbd4df9c6a8d4f12
Daniel Berliner <daniel.berliner@xaptum.com>
2019-10-28 17:12:08 -0500

    Update xap-usb-gadget to support SCM

--- buildroot-external-xaptum/package/xap-usb-gadget/xap-usb-gadget.mk
+++ buildroot-external-xaptum/package/xap-usb-gadget/xap-usb-gadget.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAP_USB_GADGET_VERSION = c5296a943fe18c9b99bed06d67177d3e19021ce5
+XAP_USB_GADGET_VERSION = 04b1554f5c63227928e104d7b86ac5708f525ccb
 XAP_USB_GADGET_SITE = ssh://git@github.com/xaptum/xaprc.git
 XAP_USB_GADGET_SITE_METHOD = git
 XAP_USB_GADGET_LICENSE = Apache-2.0
@@ -18,6 +18,10 @@ XAP_USB_GADGET_CONF_OPTS += -DXAP_PRODUCT_ID=$(BR2_PACKAGE_XAP_USB_GADGET_PRODUC
 
 ifeq ($(BR2_PACKAGE_XAP_USB_GADGET_SERIAL),y)
        XAP_USB_GADGET_CONF_OPTS += -DENABLE_SERIAL_CONSOLE=ON
+endif
+
+ifeq ($(BR2_PACKAGE_XAP_USB_GADGET_SCM),y)
+       XAP_USB_GADGET_CONF_OPTS += -DENABLE_SCM=ON
 endif
 
 $(eval $(cmake-package))
