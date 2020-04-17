5c00741249f39fe00d5f64f39b9506d83da5cb5e
Gergely Fejer <gergely.fejer@xaptum.com>
2020-04-17 00:16:09 -0500

    Updated captived to 4cdefca

--- buildroot-external-xaptum/package/captived/captived.mk
+++ buildroot-external-xaptum/package/captived/captived.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CAPTIVED_VERSION = 5f01a4e4373ebb9d87edd04733696a2be52f4b65
+CAPTIVED_VERSION = 4cdefcad4510ff8042ac20ecf940a81cba19bcde
 CAPTIVED_SITE = ssh://git@github.com/xaptum/xaprc.git
 CAPTIVED_SITE_METHOD = git
 CAPTIVED_SUPPORTS_IN_SOURCE_BUILD = NO
